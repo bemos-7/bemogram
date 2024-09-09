9253c5b8b5e7ccddab639f3934fdb8ded3813c41
bemos <bemo.0@mail.ru>
2024-09-09 21:53:39 +0300

    - создал useCase для проверки пользователя на авторизованность

---
+++ app/src/main/java/com/bemos/bemogram/domain/use_cases/IsUserAuthenticatedInFirebaseUseCase.kt
@@ -0,0 +1,10 @@
+package com.bemos.bemogram.domain.use_cases
+
+import com.bemos.bemogram.domain.interfaces.FirebaseAuthenticationRepository
+import javax.inject.Inject
+
+class IsUserAuthenticatedInFirebaseUseCase @Inject constructor(
+    private val repository: FirebaseAuthenticationRepository
+) {
+    fun execute() = repository.isUserAuthenticatedInFirebase()
+}
