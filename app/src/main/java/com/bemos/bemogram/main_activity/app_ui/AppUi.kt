29f661f85632ae2f1c2b8224e52f0c77fcbdd14e
bemos <bemo.0@mail.ru>
2024-09-10 22:07:25 +0300

    - добавил splash экран и убрали из параметров viewModel

--- app/src/main/java/com/bemos/bemogram/main_activity/app_ui/AppUi.kt
+++ app/src/main/java/com/bemos/bemogram/main_activity/app_ui/AppUi.kt
@@ -7,60 +7,85 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.bemos.bemogram.feature.profile.ProfileScreen
 import com.bemos.bemogram.feature.sign_in.SignInScreen
 import com.bemos.bemogram.feature.sign_in.vm.SignInViewModel
 import com.bemos.bemogram.feature.sign_up.SignUpScreen
 import com.bemos.bemogram.feature.sign_up.vm.SignUpViewModel
+import com.bemos.bemogram.feature.splash.SplashScreen
+import com.bemos.bemogram.utils.Constants.NAV_NAME_PROFILE
 import com.bemos.bemogram.utils.Constants.NAV_NAME_SIGN_IN
 import com.bemos.bemogram.utils.Constants.NAV_NAME_SIGN_UP
+import com.bemos.bemogram.utils.Constants.NAV_NAME_SPLASH
 
 @Composable
 fun AppUi(
     modifier: Modifier = Modifier,
     navController: NavHostController,
-    signInViewModel: SignInViewModel,
-    signUpViewModel: SignUpViewModel
 ) {
     NavHost(
         modifier = modifier,
         navController = navController,
-        startDestination = NAV_NAME_SIGN_IN
+        startDestination = NAV_NAME_SPLASH
     ) {
+        splash(
+            navController = navController
+        )
         signUp(
             navController = navController,
-            signUpViewModel = signUpViewModel
         )
         signIn(
             navController = navController,
-            signInViewModel = signInViewModel
+        )
+        profile(
+            navController = navController
+        )
+    }
+}
+private fun NavGraphBuilder.splash(
+    navController: NavController
+) {
+    composable(
+        route = NAV_NAME_SPLASH
+    ) {
+        SplashScreen(
+            navController = navController
         )
     }
 }
 
 private fun NavGraphBuilder.signUp(
     navController: NavController,
-    signUpViewModel: SignUpViewModel
+
 ) {
     composable(
         route = NAV_NAME_SIGN_UP
     ) {
         SignUpScreen(
             navController = navController,
-            signUpViewModel = signUpViewModel
         )
     }
 }
 
 private fun NavGraphBuilder.signIn(
     navController: NavController,
-    signInViewModel: SignInViewModel
+
 ) {
     composable(
         route = NAV_NAME_SIGN_IN
     ) {
         SignInScreen(
             navController = navController,
-            signInViewModel = signInViewModel
         )
+    }
+}
+
+private fun NavGraphBuilder.profile(
+    navController: NavController
+) {
+    composable(
+        route = NAV_NAME_PROFILE
+    ) {
+        ProfileScreen()
     }
 }
