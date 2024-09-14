80e0cc1f58c8a52a7cdfad77c465e4e2f4143763
bemos <bemo.0@mail.ru>
2024-09-14 19:55:05 +0300

    - создал элемент для экрана

---
+++ app/src/main/java/com/bemos/bemogram/feature/home/ui/MiniProfile.kt
@@ -0,0 +1,63 @@
+package com.bemos.bemogram.feature.home.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bemos.bemogram.domain.model.UserDomain
+
+@Composable
+fun MiniProfile(
+    user: UserDomain
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    MaterialTheme.colorScheme.background
+                )
+                .padding(5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Card(
+                modifier = Modifier.size(40.dp),
+                shape = RoundedCornerShape(1000.dp)
+            ) {}
+            Spacer(
+                modifier = Modifier.width(20.dp)
+            )
+            Text(
+                text = user.username ?: ""
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun MiniProfilePreview() {
+    MiniProfile(
+        user = UserDomain(
+            username = "Andres"
+        )
+    )
+}
