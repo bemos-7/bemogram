a5b72e03940c2ec8a092963fef01b7661d771c3f
bemos <bemo.0@mail.ru>
2024-09-16 16:07:27 +0300

    - создал кастомный TextField

---
+++ app/src/main/java/com/bemos/bemogram/feature/utils/ui/TextFieldCustom.kt
@@ -0,0 +1,32 @@
+package com.bemos.bemogram.feature.utils.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldCustom(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: @Composable (() -> Unit)? = null
+) {
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it) },
+        label = {
+            label?.let { it() }
+        },
+        shape = RoundedCornerShape(10.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
