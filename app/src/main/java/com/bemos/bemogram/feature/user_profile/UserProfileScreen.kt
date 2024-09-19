d1c1819a1da681717cc0d91e4226e243e9957f77
bemos <bemo.0@mail.ru>
2024-09-19 22:00:29 +0300

    - новый экран профиля для пользователей

---
+++ app/src/main/java/com/bemos/bemogram/feature/user_profile/UserProfileScreen.kt
@@ -0,0 +1,25 @@
+package com.bemos.bemogram.feature.user_profile
+
+import android.util.Log
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.bemos.bemogram.domain.model.UserDomain
+import com.bemos.bemogram.feature.profile.vm.ProfileViewModel
+import com.bemos.bemogram.utils.Constants.NAV_NAME_SETTINGS
+
+@Composable
+fun UserProfileScreen(
+    navController: NavController,
+    userDomain: UserDomain?
+) {
+
+    UserProfileContent(
+        userDocument = userDomain
+    )
+
+}
