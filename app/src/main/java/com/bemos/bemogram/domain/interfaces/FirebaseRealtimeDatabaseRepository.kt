26dcdeccf4c7eca48eeb20789e10cca0ffef8cde
bemos <bemo.0@mail.ru>
2024-09-19 21:57:27 +0300

    - новый репозиторий

---
+++ app/src/main/java/com/bemos/bemogram/domain/interfaces/FirebaseRealtimeDatabaseRepository.kt
@@ -0,0 +1,13 @@
+package com.bemos.bemogram.domain.interfaces
+
+interface FirebaseRealtimeDatabaseRepository {
+
+    fun createChat(
+        firstUserId: String, secondUserId: String
+    )
+
+    fun getUserChats(
+        userId: String
+    )
+
+}
