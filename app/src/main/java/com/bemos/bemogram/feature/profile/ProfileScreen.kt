4ac466503c09281b76fb9df8bbc765de19fea939
bemos <bemo.0@mail.ru>
2024-09-19 21:59:40 +0300

    - убрал импорт

--- app/src/main/java/com/bemos/bemogram/feature/profile/ProfileScreen.kt
+++ app/src/main/java/com/bemos/bemogram/feature/profile/ProfileScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.bemos.bemogram.domain.model.UserDomain
 import com.bemos.bemogram.feature.profile.vm.ProfileViewModel
 import com.bemos.bemogram.utils.Constants.NAV_NAME_SETTINGS
 
