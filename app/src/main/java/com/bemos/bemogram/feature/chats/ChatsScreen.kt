b575df8e79ffa6b6b3bc3df1eeb164f4ab38b57b
bemos <bemo.0@mail.ru>
2024-09-20 22:42:12 +0300

    - добавил navController в параметры

--- app/src/main/java/com/bemos/bemogram/feature/chats/ChatsScreen.kt
+++ app/src/main/java/com/bemos/bemogram/feature/chats/ChatsScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import com.bemos.bemogram.feature.chats.vm.ChatsViewModel
 
 @Composable
-fun ChatsScreen() {
+fun ChatsScreen(
+    navController: NavController
+) {
     val viewModel: ChatsViewModel = hiltViewModel()
 
 
