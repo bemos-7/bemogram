a94996de1269fa1e0a0b35c464085699d72ef048
bemos <bemo.0@mail.ru>
2024-09-20 22:39:24 +0300

    - добавил функцию для получения пользовательского id

--- app/src/main/java/com/bemos/bemogram/feature/user_profile/vm/UserProfileViewModel.kt
+++ app/src/main/java/com/bemos/bemogram/feature/user_profile/vm/UserProfileViewModel.kt
@@ -2,13 +2,19 @@ package com.bemos.bemogram.feature.user_profile.vm
 
 import androidx.lifecycle.ViewModel
 import com.bemos.bemogram.domain.use_cases.CreateChatUseCase
+import com.bemos.bemogram.domain.use_cases.GetUserUidUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class UserProfileViewModel @Inject constructor(
     private val createChatUseCase: CreateChatUseCase,
+    private val getUserUidUseCase: GetUserUidUseCase
 ) : ViewModel() {
+
+    val userIdFlow = MutableStateFlow("")
 
     fun createChat(
         firstUserId: String,
@@ -18,6 +24,13 @@ class UserProfileViewModel @Inject constructor(
             firstUserId = firstUserId,
             secondUserId = secondUserId
         )
+    }
+
+    fun getUserUid() {
+        val userId = getUserUidUseCase.execute()
+        userIdFlow.update {
+            userId
+        }
     }
 
 }
