7ac049d372fce07d185bc951a51590fb90f9b977
bemos <bemo.0@mail.ru>
2024-09-21 22:28:11 +0300

    - useCase для функции

---
+++ app/src/main/java/com/bemos/bemogram/domain/use_cases/ListenForMessagesUseCase.kt
@@ -0,0 +1,18 @@
+package com.bemos.bemogram.domain.use_cases
+
+import com.bemos.bemogram.domain.interfaces.FirebaseRealtimeDatabaseRepository
+import com.bemos.bemogram.domain.model.MessageDomain
+import javax.inject.Inject
+
+class ListenForMessagesUseCase @Inject constructor(
+    private val repository: FirebaseRealtimeDatabaseRepository
+) {
+    fun execute(chatId: String, onNewMessage: (MessageDomain) -> Unit) {
+        repository.listenForMessages(
+            chatId = chatId,
+            onNewMessage = {
+                onNewMessage(it)
+            }
+        )
+    }
+}
