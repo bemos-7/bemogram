893c06f88343ed9e79a90c33a2275040b68b6a25
bemos <bemo.0@mail.ru>
2024-09-25 19:57:19 +0300

    - добавил переход

--- app/src/main/java/com/bemos/bemogram/feature/user_profile/UserProfileScreen.kt
+++ app/src/main/java/com/bemos/bemogram/feature/user_profile/UserProfileScreen.kt
@@ -11,7 +11,9 @@ import androidx.navigation.NavController
 import com.bemos.bemogram.domain.model.UserDomain
 import com.bemos.bemogram.feature.profile.vm.ProfileViewModel
 import com.bemos.bemogram.feature.user_profile.vm.UserProfileViewModel
+import com.bemos.bemogram.utils.Constants.NAV_NAME_CHATS
 import com.bemos.bemogram.utils.Constants.NAV_NAME_SETTINGS
+import com.bemos.bemogram.utils.Constants.NAV_NAME_USER_CHAT
 
 @Composable
 fun UserProfileScreen(
@@ -29,6 +31,7 @@ fun UserProfileScreen(
                 firstUserId = userId,
                 secondUserId = it.userId!!
             )
+            navController.navigate(NAV_NAME_CHATS)
         }
     )
 
