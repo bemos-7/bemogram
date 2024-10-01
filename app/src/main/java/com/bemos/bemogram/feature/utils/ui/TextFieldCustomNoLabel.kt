4d4841979b72cf43cd8b53b775a04c67f43d4f4c
bemos <bemo.0@mail.ru>
2024-10-01 18:54:00 +0300

    - новое кастомное поле для заполнения

---
+++ app/src/main/java/com/bemos/bemogram/feature/utils/ui/TextFieldCustomNoLabel.kt
@@ -0,0 +1,32 @@
+package com.bemos.bemogram.feature.utils.ui
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextFieldCustomNoLabel(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    TextField(
+        modifier = modifier,
+        value = value,
+        onValueChange = { onValueChange(it) },
+        shape = RoundedCornerShape(15.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        visualTransformation = VisualTransformation.None
+    )
+}
