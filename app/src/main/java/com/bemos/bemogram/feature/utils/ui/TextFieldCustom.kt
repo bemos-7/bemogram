cc9f0d2abf7aa62ff067234353d744eec529f979
bemos <bemo.0@mail.ru>
2024-10-06 00:05:25 +0300

    - поменял textFieldColors на colors в соответствии с новой версией

--- app/src/main/java/com/bemos/bemogram/feature/utils/ui/TextFieldCustom.kt
+++ app/src/main/java/com/bemos/bemogram/feature/utils/ui/TextFieldCustom.kt
@@ -24,7 +24,7 @@ fun TextFieldCustom(
             label?.let { it() }
         },
         shape = RoundedCornerShape(15.dp),
-        colors = TextFieldDefaults.textFieldColors(
+        colors = TextFieldDefaults.colors(
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
         )
