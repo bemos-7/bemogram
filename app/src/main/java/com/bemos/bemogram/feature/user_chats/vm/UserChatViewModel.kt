0147534b6c12d0495f36ef9103852d5bf3ebfbd6
bemos <bemo.0@mail.ru>
2024-10-07 21:54:44 +0300

    - добавил в параметры useCase пуш уведомлений, и реализовал функцию

--- app/src/main/java/com/bemos/bemogram/feature/user_chats/vm/UserChatViewModel.kt
+++ app/src/main/java/com/bemos/bemogram/feature/user_chats/vm/UserChatViewModel.kt
@@ -1,20 +1,26 @@
 package com.bemos.bemogram.feature.user_chats.vm
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.bemos.bemogram.domain.model.MessageDomain
+import com.bemos.bemogram.domain.model.PushNotification
 import com.bemos.bemogram.domain.use_cases.GetUserUidUseCase
 import com.bemos.bemogram.domain.use_cases.ListenForMessagesUseCase
 import com.bemos.bemogram.domain.use_cases.SendMessageUseCase
+import com.bemos.bemogram.domain.use_cases.SendPushNotificationUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class UserChatViewModel @Inject constructor(
     private val sendMessageUseCase: SendMessageUseCase,
     private val listenForMessagesUseCase: ListenForMessagesUseCase,
-    private val getUserUidUseCase: GetUserUidUseCase
+    private val getUserUidUseCase: GetUserUidUseCase,
+    private val sendPushNotificationUseCase: SendPushNotificationUseCase
 ) : ViewModel() {
 
     val messages = MutableStateFlow<List<MessageDomain>>(listOf())
@@ -50,6 +56,18 @@ class UserChatViewModel @Inject constructor(
                 }
             }
         )
+    }
+
+    fun sendPushNotification(
+        notification: PushNotification
+    ) = viewModelScope.launch {
+        val result = sendPushNotificationUseCase.execute(notification)
+        result.onSuccess {
+            Log.d("NotificationSend", "successfully")
+
+        }.onFailure {
+            Log.d("NotificationSend", it.message.toString())
+        }
     }
 
 }
